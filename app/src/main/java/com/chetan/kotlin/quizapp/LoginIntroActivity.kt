5f02c95610e3ff4ca580db480f7e1464a12b43e6
cbbchetan <cbb.chetan@gmail.com>
2021-07-20 14:29:43 +0530

    Initial commit

---
+++ app/src/main/java/com/chetan/kotlin/quizapp/LoginIntroActivity.kt
@@ -0,0 +1,11 @@
+package com.chetan.kotlin.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginIntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_intro)
+    }
+}
